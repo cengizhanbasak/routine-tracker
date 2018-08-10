6adf316a4c2e386f4f73a50372c4de143b4155e2
Cengizhan Başak <cengizhanbasakk@gmail.com>
2018-08-10 09:03:37 +0300

    can list active routine forms

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,46 +1,52 @@
 import React, { Component } from 'react';
 import './TaskList.css';
 
+const axios = require('axios');
+
 class TaskList extends Component {
+    constructor (props){
+        super(props);
+        this.state={ routineForms:[] }
+    }
+
+    componentDidMount() {
+        axios({
+            method: 'GET',
+            url: 'https://api.jotform.com/user/forms?apikey=85dcbbcdad0b18a508112756e56fdcfb',
+        }).then((response) => {
+            this.setState({routineForms:response.data.content.filter(
+                (form) => form.title.indexOf("{DailyRoutine}") !== -1 && form.status === "ENABLED" )});
+        })
+    }
 
     render()
     {
         return (
             <ul className="Dashboard-list">
-                <li className="taskItem">
-                    <div className="name">
-                        Stay Awesome
-                    </div>
-                    <div className="time">
-                        12:30
-                    </div>
-                    <div className="recentProgress">
-                        <div className="dayProgress done">Done</div>
-                        <div className="dayProgress done">Done</div>
-                        <div className="dayProgress done">Done</div>
-                        <div className="dayProgress fail">Fail</div>
-                        <div className="dayProgress inProgress">-</div>
-                        <div className="dayProgress inProgress">-</div>
-                        <div className="dayProgress inProgress">-</div>
-                    </div>
-                </li>
-                <li className="taskItem">
-                    <div className="name">
-                        Workout
-                    </div>
-                    <div className="time">
-                        16:30
-                    </div>
-                    <div className="recentProgress">
-                        <div className="dayProgress done">Done</div>
-                        <div className="dayProgress fail">Fail</div>
-                        <div className="dayProgress done">Done</div>
-                        <div className="dayProgress fail">Fail</div>
-                        <div className="dayProgress fail">Fail</div>
-                        <div className="dayProgress done">Done</div>
-                        <div className="dayProgress inProgress">-</div>
-                    </div>
-                </li>
+                {
+                    this.state.routineForms.map((form)=> {
+                        return (
+                            <li className="taskItem">
+                                <div className="name">
+                                    {form.title}
+                                </div>
+                                <div className="time">
+                                    12:30
+                                </div>
+                                <div className="recentProgress">
+                                    <div className="dayProgress done">Done</div>
+                                    <div className="dayProgress done">Done</div>
+                                    <div className="dayProgress done">Done</div>
+                                    <div className="dayProgress fail">Fail</div>
+                                    <div className="dayProgress inProgress">-</div>
+                                    <div className="dayProgress inProgress">-</div>
+                                    <div className="dayProgress inProgress">-</div>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+
             </ul>
         )
     }
