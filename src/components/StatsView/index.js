b901b0a3084930bbcad0c3bea90525edf9e31b9b
Cengizhan Başak <cengizhanbasakk@gmail.com>
2018-09-05 17:16:26 +0300

    achievements added

--- src/components/StatsView/index.js
+++ src/components/StatsView/index.js
@@ -4,9 +4,27 @@ import './StatsView.css';
 
 class StatsView extends Component {
 
-    render(){
+    constructor(props){
+        super(props);
         var totalRecords = 0;
         this.props.routinesList.forEach((routine)=> totalRecords += parseInt(routine.count,10) )
+        this.state={ achievements:0, totalRecords  }
+
+    }
+
+    componentDidMount(){
+        if(this.state.totalRecords >= 5){
+            this.setState({ achievements: this.state.achievements+1 })
+        }
+        if(this.state.totalRecords >= 10){
+            this.setState({ achievements: this.state.achievements+2 })
+        }
+        if(this.state.totalRecords >= 50){
+            this.setState({ achievements: this.state.achievements+1 })
+        }
+    }
+
+    render(){
 
         return (
             <div className="Stats-page">
@@ -16,13 +34,43 @@ class StatsView extends Component {
                         <h1>Routines Tracking</h1>
                     </div>
                     <div className="col taskFill">
-                        <h1 className="big">{totalRecords}</h1>
+                        <h1 className="big">{this.state.totalRecords}</h1>
                         <h1>Total Routine Records</h1>
                     </div>
                     <div className="col taskPercent">
-                        <h1 className="big">0</h1>
+                        <h1 className="big">{this.state.achievements}</h1>
                         <h1>Achievements</h1>
                     </div>
+                </div>
+                <div className="achievements-section">
+                    <h1> Achievements </h1>
+                    <ul className="achievements-list">
+                    {
+                        this.state.totalRecords>=5
+                        &&
+                        <li>
+                            <div classNmae="name">Beginner</div>
+                            <div className="descr"> Record 5 times on routines </div>
+                        </li>
+                    }
+                    {
+                        this.state.totalRecords>=10
+                        &&
+                        <li>
+                            <div classNmae="name">Intermediate</div>
+                            <div className="descr"> Record 10 times on routines </div>
+                        </li>
+                    }
+                    {
+                        this.state.totalRecords>=50
+                        &&
+                        <li>
+                            <div classNmae="name">Pro</div>
+                            <div className="descr"> Record 50 times on routines </div>
+                        </li>
+                    }
+                    </ul>
+
                 </div>
             </div>
         )
